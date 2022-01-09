9ba347d87fd83ab6e1ed932761bf6485008d34bb
Rozan Rosandi <rozanxt@gmail.com>
2022-01-09 16:03:35 +0100

    Added required libraries to the git repository
    Note: There is an issue with org.lwjgl.input.Mouse.setGrabbed
          when launching the .jar file from the operating system.

--- src/zan/game/sound/SoundManager.java
+++ src/zan/game/sound/SoundManager.java
@@ -6,50 +6,47 @@ import java.net.URL;
 import paulscode.sound.SoundSystem;
 import paulscode.sound.SoundSystemConfig;
 import paulscode.sound.SoundSystemException;
-import paulscode.sound.SoundSystemJPCT;
 import paulscode.sound.codecs.CodecJOrbis;
-import paulscode.sound.libraries.LibraryJavaSound;
 import paulscode.sound.libraries.LibraryLWJGLOpenAL;
 import zan.game.resource.ResourceData;
 
 /** Sound manager class */
 public class SoundManager {
 	private static final String LOGNAME = "SoundManager :: ";
-	
+
 	/** Sound resources directory */
 	private final static String SND_DIR = "res/snd/";
-	
+
 	/** Sound system */
 	private static SoundSystem sndSystem;
-	
+
 	/** Current background music */
 	private static String backgroundMusic;
-	
+
 	/** Initialize sound manager */
 	public static void init() {
 		try {
 			SoundSystemConfig.addLibrary(LibraryLWJGLOpenAL.class);
-			SoundSystemConfig.addLibrary(LibraryJavaSound.class);
 			SoundSystemConfig.setCodec("wav", CodecJOrbis.class);
 			SoundSystemConfig.setCodec("ogg", CodecJOrbis.class);
 		} catch(SoundSystemException e) {
 		    System.err.println(LOGNAME + "Error linking with sound plug-ins:\n " + e);
 		}
-		sndSystem = new SoundSystemJPCT();
+		sndSystem = new SoundSystem();
 	}
-	
+
 	/** Sound manager cleanup */
 	public static void destroy() {
 		sndSystem.cleanup();
 	}
-	
+
 	/** Load sound from a resource data */
 	public static void loadSoundData(ResourceData sndData) {
 		if (sndData.isEmpty()) {
 			System.err.println(LOGNAME + "Error loading sounds:\n no data found");
 			return;
 		}
-		
+
 		for (int i=0;i<sndData.getNumNodes();i++) {
 			ResourceData node = sndData.getNode(i);
 			if (!node.isEmpty()) {
@@ -60,7 +57,7 @@ public class SoundManager {
 			}
 		}
 	}
-	
+
 	/** Load sound */
 	private static void loadSound(String name, String fnm, boolean stream, boolean loop) {
 		try {
@@ -74,32 +71,32 @@ public class SoundManager {
 			System.err.println(LOGNAME + "Error loading sound: " + SND_DIR + fnm);
 		}
 	}
-	
+
 	/** Set sound volume */
 	public static void setVolume(String name, float volume) {
 		sndSystem.setVolume(name, volume);
 	}
-	
+
 	/** Play sound effect */
 	public static void playSFX(String name) {
 		if (sndSystem.playing(name)) sndSystem.stop(name);
 		sndSystem.play(name);
 	}
-	
+
 	/** Play background music */
 	public static void playBGM(String name) {
 		if (sndSystem.playing(backgroundMusic)) sndSystem.stop(backgroundMusic);
 		backgroundMusic = name;
 		sndSystem.play(backgroundMusic);
 	}
-	
+
 	/** Unload sounds from a resource data */
 	public static void unloadSoundData(ResourceData sndData) {
 		if (sndData.isEmpty()) {
 			System.err.println(LOGNAME + "Error unloading sounds: no data found");
 			return;
 		}
-		
+
 		for (int i=0;i<sndData.getNumNodes();i++) {
 			ResourceData node = sndData.getNode(i);
 			if (!node.isEmpty()) {
@@ -107,10 +104,10 @@ public class SoundManager {
 			}
 		}
 	}
-	
+
 	/** Unload sound */
 	private static void unloadSound(String name) {
 		sndSystem.unloadSound(name);
 	}
-	
+
 }
--- src/zan/game/sprite/TextureManager.java
+++ src/zan/game/sprite/TextureManager.java
@@ -1,5 +1,20 @@
 package zan.game.sprite;
 
+import static org.lwjgl.opengl.GL11.GL_LINEAR;
+import static org.lwjgl.opengl.GL11.GL_RGBA;
+import static org.lwjgl.opengl.GL11.GL_RGBA8;
+import static org.lwjgl.opengl.GL11.GL_TEXTURE_2D;
+import static org.lwjgl.opengl.GL11.GL_TEXTURE_MAG_FILTER;
+import static org.lwjgl.opengl.GL11.GL_TEXTURE_MIN_FILTER;
+import static org.lwjgl.opengl.GL11.GL_TEXTURE_WRAP_S;
+import static org.lwjgl.opengl.GL11.GL_TEXTURE_WRAP_T;
+import static org.lwjgl.opengl.GL11.GL_UNSIGNED_BYTE;
+import static org.lwjgl.opengl.GL11.glBindTexture;
+import static org.lwjgl.opengl.GL11.glDeleteTextures;
+import static org.lwjgl.opengl.GL11.glGenTextures;
+import static org.lwjgl.opengl.GL11.glTexImage2D;
+import static org.lwjgl.opengl.GL11.glTexParameteri;
+
 import java.awt.AlphaComposite;
 import java.awt.Graphics2D;
 import java.awt.GraphicsConfiguration;
@@ -18,30 +33,28 @@ import org.lwjgl.opengl.GL12;
 
 import zan.game.resource.ResourceData;
 
-import static org.lwjgl.opengl.GL11.*;
-
 /** Texture manager class */
 public class TextureManager {
 	private static final String LOGNAME = "TextureManager :: ";
-	
+
 	/** Texture resources directory */
 	private static final String IMG_DIR = "res/img/";
-	
+
 	/** Texture storage */
 	private static HashMap<String, Integer> textureStore;
-	
+
 	/** Graphics configuration */
 	private static GraphicsConfiguration gc;
 	private static final int BYTES_PER_PIXEL = 4;
-	
+
 	/** Initialize texture manager */
 	public static void init() {
 		textureStore = new HashMap<String, Integer>();
-		
+
 		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
 		gc = ge.getDefaultScreenDevice().getDefaultConfiguration();
 	}
-	
+
 	/** Texture manager cleanup */
 	public static void destroy() {
 		for (Map.Entry<String, Integer> entry : textureStore.entrySet()) {
@@ -49,14 +62,14 @@ public class TextureManager {
 		}
 		textureStore.clear();
 	}
-	
+
 	/** Load textures from a resource data */
 	public static void loadTextureData(ResourceData texData) {
 		if (texData.isEmpty()) {
 			System.err.println(LOGNAME + "Error loading texture data:\n no data found");
 			return;
 		}
-		
+
 		for (int i=0;i<texData.getNumNodes();i++) {
 			ResourceData node = texData.getNode(i);
 			if (node.getNumValues() == 1) {
@@ -64,7 +77,7 @@ public class TextureManager {
 			}
 		}
 	}
-	
+
 	/** Store texture */
 	private static boolean storeTexture(String name, String fnm) {
 		if (name == null || fnm == null) {
@@ -75,40 +88,40 @@ public class TextureManager {
 			System.err.println(LOGNAME + "Error loading texture:\n " + name + " is already used");
 			return false;
 		}
-		
+
 		int textureID = loadTexture(fnm);
 		if (textureID != 0) {
-			textureStore.put(name, new Integer(textureID));
+			textureStore.put(name, textureID);
 			return true;
 		} else return false;
 	}
-	
+
 	/** Load texture */
 	private static int loadTexture(String fnm) {
 		try {
 			BufferedImage im = ImageIO.read(new File(IMG_DIR + fnm));
-			
+
 			int transparency = im.getColorModel().getTransparency();
 			BufferedImage bi =  gc.createCompatibleImage(im.getWidth(), im.getHeight(), transparency);
 			Graphics2D g2d = bi.createGraphics();
 			g2d.setComposite(AlphaComposite.Src);
-			
+
 			g2d.drawImage(im, 0, 0, null);
 			g2d.dispose();
 			return genTexture(bi);
 		} catch(IOException e) {
-			System.err.println(LOGNAME + "Error loading texture for " + IMG_DIR + fnm + ":\n " + e); 
+			System.err.println(LOGNAME + "Error loading texture for " + IMG_DIR + fnm + ":\n " + e);
 			return 0;
 		}
 	}
-	
+
 	/** Generate texture ID */
 	private static int genTexture(BufferedImage image) {
 		int[] pixels = new int[image.getWidth() * image.getHeight()];
 		image.getRGB(0, 0, image.getWidth(), image.getHeight(), pixels, 0, image.getWidth());
-		
+
 		ByteBuffer buffer = BufferUtils.createByteBuffer(image.getWidth() * image.getHeight() * BYTES_PER_PIXEL);
-		
+
 		for(int y = 0; y < image.getHeight(); y++){
 			for(int x = 0; x < image.getWidth(); x++){
 				int pixel = pixels[y * image.getWidth() + x];
@@ -118,33 +131,33 @@ public class TextureManager {
 				buffer.put((byte) ((pixel >> 24) & 0xFF));	// Alpha component. Only for RGBA
 			}
 		}
-		
+
 		buffer.flip();
-		
+
 		int textureID = glGenTextures();			// Generate texture ID
 		glBindTexture(GL_TEXTURE_2D, textureID);	// Bind texture ID
-		
+
 		// Setup wrap mode
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL12.GL_CLAMP_TO_EDGE);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL12.GL_CLAMP_TO_EDGE);
-		
+
 		// Setup texture scaling filtering
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR); //GL_LINEAR / GL_NEAREST
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-		
+
 		// Send texel data to OpenGL
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, image.getWidth(), image.getHeight(), 0, GL_RGBA, GL_UNSIGNED_BYTE, buffer);
-		
+
 		return textureID;
 	}
-	
+
 	/** Unload textures from a resource data */
 	public static void unloadTextureData(ResourceData texData) {
 		if (texData.isEmpty()) {
 			System.err.println(LOGNAME + "Error unloading textures:\n no data found");
 			return;
 		}
-		
+
 		for (int i=0;i<texData.getNumNodes();i++) {
 			ResourceData node = texData.getNode(i);
 			if (!node.isEmpty()) {
@@ -152,7 +165,7 @@ public class TextureManager {
 			}
 		}
 	}
-	
+
 	/** Unload texture */
 	private static boolean unloadTexture(String name) {
 		if (isTextureLoaded(name)) {
@@ -161,20 +174,20 @@ public class TextureManager {
 			return true;
 		} else return false;
 	}
-	
+
 	/** @return Texture ID */
 	public static int getTextureID(String name) {
 		if (!isTextureLoaded(name)) {
-			System.out.println(LOGNAME + "No texture stored under " + name);  
+			System.out.println(LOGNAME + "No texture stored under " + name);
 			return 0;
 		}
-		return (int)textureStore.get(name);
+		return textureStore.get(name);
 	}
-	
+
 	/** @return true if texture is loaded */
 	public static boolean isTextureLoaded(String name) {
 		if (textureStore.get(name) == null || textureStore.get(name) == 0) return false;
 		return true;
 	}
-	
+
 }
